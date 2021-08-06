830a7e9f2d652e0a3e2fbf4cb3767e9f0052c699
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-06 20:59:30 -0300

    CTD

--- url-shortner/src/App.vue
+++ url-shortner/src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <v-app>
-    <v-app-bar color="blue-grey lighten-5" class="d-flex flex-column">
-      <v-text-field
-            value="John Doe"
-            label="Solo"
-            solo
-            disabled
-          ></v-text-field>
-      <v-btn block elevation="2" text x-large>Short</v-btn>
+    <v-app-bar
+      color="
+    blue-grey lighten-5"
+      class=""
+    >
+      <v-container>
+        <v-row>
+          <v-col
+          cols="12"
+          sm="6">
+          <v-textarea
+            prepend-inner-icon="mdi-comment"
+            class="mx-2"
+            label="Your URL here"
+            rows="1"
+          ></v-textarea>
+          <v-btn block elevation="2" text x-large>Short</v-btn>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-app-bar>
   </v-app>
 </template>
