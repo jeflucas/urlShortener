e6f7f26f71944e4f9ca46f056357ddfa62aad921
jeflucas <jef.lucas@hotmail.co.uk>
2021-08-08 22:01:45 -0300

    CTD

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-function shortUrl(){
+var longUrl = document.getElementById("longUrl");
+const cut = document.getElementById("cut");
+const btn = document.getElementById("btn");
+const apiKey = "ff6e019332372e9320ab30c0461ac9f92276eff7";
 
-    var id = "o_4qjue1o0cr";
-    var url = "https://api-ssl.bitly.com/v4/shorten";
-    
-    let request = new XMLHttpRequest();
-    request.open("GET", url, true);
-    
-    request.onreadystatechange = function () {
-      if (request.readyState == 4 && request.status == 200)
-        console.log(request.responseText);
-    };
-    
-    request.send();
+function shortThat() {
+  const info = { long_url: longUrl.value };
+  fetch("https://api-ssl.bitly.com/v4/shorten", {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(info),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then((json) => {
+      document.getElementById("shortUrl").value = json.link;
+    });
 }
 
-shortUrl()
+btn.addEventListener("click", shortThat);
+document.querySelector("#longUrl").addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    shortThat();
+  }
+});
 
-// https://github.com/LaraSabatini/link-shortener
+function copyThat() {
+  var copyUrl = document.getElementById("shortUrl");
+  copyUrl.select();
+  copyUrl.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  alert("Short Url copied");
+}
 
-// https://github.com/Amansingh1202/link-shortener ver mais tarde
-// https://github.com/andy3278/URLshorten
+cut.addEventListener("click", copyThat);
